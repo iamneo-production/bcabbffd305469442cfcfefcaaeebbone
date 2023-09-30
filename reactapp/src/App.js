9e1472087e4e23366cefea7aa61d77592326c156
Iamneo <techadmin@iamneo.ai>
2023-09-30 05:03:00 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -12,14 +12,3 @@ function App() {
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
